3244e85763122ef2b0ca3c01eb235f3a579e69df
freedomhero <freedomhero2008@gmail.com>
2020-08-06 22:28:14 +0800

    tmp

---
+++ tests/fixture/autoCompile.js
@@ -0,0 +1,34 @@
+const { compile } = require('scryptjs');
+const glob = require('glob');
+const { join } = require('path');
+const { exit } = require('process');
+
+function compileAllContracts() {
+  const contracts = glob.sync(join(__dirname, '../../contracts/*.scrypt'));
+  contracts.forEach(filePath => {
+    compileContract(filePath);
+  })
+}
+
+function compileContract(filePath) {
+  console.log(`Compiling contract ${filePath} ...`)
+
+  const result = compile(
+    { path: filePath },
+    { desc: true, outputDir: join(__dirname, 'autoGen') }
+  );
+
+  if (result.errors.length > 0) {
+    console.log(`Contract ${filePath} compiling failed with errors:`);
+    console.log(result.errors);
+    exit(1);
+  }
+}
+
+const args = process.argv;
+
+if (args.includes('--single')) {
+  compileContract(args[args.length - 1]);
+} else {
+  compileAllContracts();
+}
